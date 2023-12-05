4e4bde45f3c76189c0a7284dacb86192d385c719
jackwise254 <jackwise179@gmail.com>
2023-12-04 20:55:57 -0800

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,9 +2,3 @@
 def print_list_integer(my_list=[]):
     for number in my_list:
         print("{}".format(number))
-
-
-if __name__ == "__main__":
-    # Example usage
-    my_list = [1, 2, 3, 4, 5]
-    print_list_integer(my_list)
